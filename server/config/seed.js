300c4065f9fb6dab903e5ae6fa35ac3bebdcd9bc
brostone51 <stone.525@osu.edu>
2015-07-13 14:26:31 -0400

    Small change to seed file so the sample events look better.

--- server/config/seed.js
+++ server/config/seed.js
@@ -9,67 +9,45 @@ var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 var Events = require('../api/events/events.model');
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
-
-User.find({}).remove(function() {
+User.find({}).remove(function () {
   User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
+      provider: 'local',
+      name: 'Test User',
+      email: 'test@test.com',
+      password: 'test'
+    }, {
+      provider: 'local',
+      role: 'admin',
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: 'admin'
+    }, function () {
       console.log('finished populating users');
     }
   );
 });
 
-Events.find({}).remove(function() {
+Events.find({}).remove(function () {
   Events.create({
-    name: 'test1',
-    description: 'A lunch for anyone who wants to come!',
-    date: '2015-07-24',
-    time: '11:30',
-    location: 'test1',
-    meetingLocation: '13th kitchen',
-    meetingTime: '11:15',
-    attendees: ['Joe'],
-    createdBy: 'Joe'
-  },
-    {
-      name: 'test2',
-      description: 'A lunch for people who are on my team!',
-      date: '2015-07-25',
+      name: 'WALNUT WEDNESDAY!',
+      description: 'LETS GET OUR FOOD TRUCK ON WITH SOME DELICIOUS TACOS!',
+      date: '2015-07-22',
       time: '11:45',
-      location: 'test2',
-      meetingLocation: '12th kitchen',
-      meetingTime: '11:40',
-      attendees: ['Alan'],
-      createdBy: 'Alan'
+      location: 'Walnut street',
+      meetingLocation: '1st floor lobby',
+      meetingTime: '11:30',
+      attendees: ['Test User'],
+      createdBy: 'Test User'
+    },
+    {
+      name: 'PIZZA TIME!!!',
+      description: 'Lets go get vincenzas!',
+      date: '2015-07-21',
+      time: '12:00',
+      location: 'Vincenza\'s',
+      meetingLocation: '13th kitchen',
+      meetingTime: '11:55',
+      attendees: ['Test User'],
+      createdBy: 'Test User'
     });
 });
